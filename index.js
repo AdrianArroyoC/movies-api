6a0451abf976d03476c11ac33fdfe9f5a0553d3e
AdrianArroyoC <adrianarroyoceja@gmail.com>
2020-06-08 22:56:06 -0500

    Solución del reto con Request y Response Objects

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
 
 const { config } = require('./config/index');
 
@@ -10,6 +12,24 @@ app.get('/', (req, res) => {
 
 app.get('/json', (req, res) => {
   res.json({ hello: 'world' });
+});
+
+// Reto con Query Params
+app.get('/query', (req, res) => {
+  const leap = req.query.year % 4 ? "isn't" : 'is';
+  res.send(`${req.query.year} ${leap} leap-year.`);
+});
+
+// Reto con URL Params
+app.get('/params/:year', (req, res) => {
+  const leap = req.params.year % 4 ? "isn't" : 'is';
+  res.send(`${req.params.year} ${leap} leap-year.`);
+});
+
+// Reto con el Body
+app.post('/body', (req, res) => {
+  const leap = req.body.year % 4 ? "isn't" : 'is';
+  res.send(`${req.body.year} ${leap} leap-year.`);
 });
 
 app.listen(config.port, () => {
